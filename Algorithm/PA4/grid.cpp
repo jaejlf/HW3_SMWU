945e8fc4609c121468a9349d82374e733d28a2e6
jaejeong <78673570+jaejlf@users.noreply.github.com>
2021-12-17 12:49:21 +0900

    first dot 조건 추가 및 함수 병합

--- Algorithm/PA4/grid.cpp
+++ Algorithm/PA4/grid.cpp
@@ -5,33 +5,38 @@ unsigned long long grid::numOfWays(void){
 	dotNum = (n + 1) * (n + 1);
 	targetDot = (n + 1) * (n + 1) - 1;
 
-	initBesideDots();
 	initStatus();
 	findAllPath(0);
 
 	return result;
 }
 
-void grid::initBesideDots(){
+void grid::initStatus(){
+  	//init beside
 	beside = new int *[dotNum];
-
 	for (int i = 0; i < dotNum; i++){
 		beside[i] = new int[4];
-
 		beside[i][TOP] = i - (n + 1);
 		beside[i][DOWN] = i + (n + 1);
 		beside[i][LEFT] = i - 1;
 		beside[i][RIGHT] = i + 1;
 
-		if (beside[i][TOP] < 0)	{ beside[i][TOP] = NOTHING; }
-		if (beside[i][DOWN] >= dotNum) { beside[i][DOWN] = NOTHING; }
-		if (i % (n + 1) == 0) { beside[i][LEFT] = NOTHING; }
-		if (beside[i][RIGHT] % (n + 1) == 0) { beside[i][RIGHT] = NOTHING; }
+		if (beside[i][TOP] < 0){
+      beside[i][TOP] = NOTHING;
+		}
+		if (beside[i][DOWN] >= dotNum){
+			beside[i][DOWN] = NOTHING;
+		}
+		if (i % (n + 1) == 0){
+			beside[i][LEFT] = NOTHING;
+		}
+		if (beside[i][RIGHT] % (n + 1) == 0){
+			beside[i][RIGHT] = NOTHING;
+		}
 	}
-}
 
-void grid::initStatus(){
-	status = new int[dotNum];
+  //init status
+  status = new int[dotNum];
 	memset(status, NOT_VISITED, dotNum * sizeof(int));
 	status[0] = VISITED;
 }
@@ -42,12 +47,24 @@ void grid::findAllPath(int current){
 		return;
 	}
 
-	for (int i = 0; i < 4; i++) {
-		int nextDot = beside[current][i];
-		if ((nextDot != NOTHING) && (status[nextDot] == NOT_VISITED)){
-			status[nextDot] = VISITED;
-			findAllPath(nextDot);
-			status[nextDot] = NOT_VISITED;
+	if (current == 0){ //first dot
+		for (int i = 2; i < 4; i++){
+			int nextDot = beside[current][i];
+			if ((nextDot != NOTHING) && (status[nextDot] == NOT_VISITED)){
+				status[nextDot] = VISITED;
+				findAllPath(nextDot);
+				status[nextDot] = NOT_VISITED;
+			}
+		}
+	}
+	else{
+		for (int i = 0; i < 4; i++){
+			int nextDot = beside[current][i];
+			if ((nextDot != NOTHING) && (status[nextDot] == NOT_VISITED)){
+				status[nextDot] = VISITED;
+				findAllPath(nextDot);
+				status[nextDot] = NOT_VISITED;
+			}
 		}
 	}
 }

c5eabefc56aa1ff19e766cf4afd1f205c80d417b
jaejeong <78673570+jaejlf@users.noreply.github.com>
2021-12-18 17:09:31 +0900

    Add JUnit Test Case

---
+++ Clean Code/Homework2/src/SortArrTest.java
@@ -0,0 +1,24 @@
+package Homework2.src;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SortArrTest {
+
+	@Test
+	void testSortNumbersUsingBubble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSortNumbersUsingQuick() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSortNumbersUsingInsertion() {
+		fail("Not yet implemented");
+	}
+
+}

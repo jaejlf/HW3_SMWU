e158b0eedc25c42f204307f5f296468f484b3bd0
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-06-07 23:17:16 +0900

    DBP - Final Project

--- Database Programming/Final_Project/final_project/src/main/webapp/sql/table/running_movie_table.sql
+++ Database Programming/Final_Project/final_project/src/main/webapp/sql/table/running_movie_table.sql
@@ -8,3 +8,7 @@ CREATE TABLE RUNNING_MOVIE_TABLE
     movie_date          DATE            NOT NULL, 
     PRIMARY KEY (running_movie_id)
 );
+
+ALTER TABLE RUNNING_MOVIE_TABLE
+    ADD CONSTRAINT RMT_movie_id_FK FOREIGN KEY (movie_id)
+        REFERENCES MOVIE_INFO_TABLE (movie_id);
--- Database Programming/Final_Project/final_project/src/main/webapp/sql/table/user_booking_table.sql
+++ Database Programming/Final_Project/final_project/src/main/webapp/sql/table/user_booking_table.sql
@@ -7,3 +7,15 @@ CREATE TABLE USER_BOOKING_TABLE
     seat_id             VARCHAR2(5)     NOT NULL, 
     PRIMARY KEY (booking_id)
 );
+
+ALTER TABLE USER_BOOKING_TABLE
+    ADD CONSTRAINT UBT_user_id_FK FOREIGN KEY (user_id)
+        REFERENCES USER_TABLE (user_id);
+
+ALTER TABLE USER_BOOKING_TABLE
+    ADD CONSTRAINT UBT_running_movie_id_FK FOREIGN KEY (running_movie_id)
+        REFERENCES RUNNING_MOVIE_TABLE (running_movie_id);
+
+ALTER TABLE USER_BOOKING_TABLE
+    ADD CONSTRAINT UFK_theater_seat_id_FK FOREIGN KEY (theater_id, seat_id)
+        REFERENCES SEAT_INFO_TABLE (theater_id, seat_id);
